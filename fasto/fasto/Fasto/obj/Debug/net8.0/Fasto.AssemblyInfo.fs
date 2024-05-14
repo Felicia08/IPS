661e35582cea6f80d7abb33b1e99270c36f44dc1
djafar1 <djafar1@hotmail.dk>
2024-05-14 18:16:52 +0200

    We tried

--- fasto/fasto/Fasto/obj/Debug/net8.0/Fasto.AssemblyInfo.fs
+++ fasto/fasto/Fasto/obj/Debug/net8.0/Fasto.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5414496e3eb26b2286d414e499421565371650fb")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+732dd2519486e09e090a7bf2ce80d579058f66dc")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
