a680581cdfdae26200a33695b85d23aa2e669445
djafar1 <djafar1@hotmail.dk>
2024-05-21 13:13:03 +0200

    Fikset lidt hist og dist

--- fasto/fasto/Fasto/obj/Debug/net8.0/Fasto.AssemblyInfo.fs
+++ fasto/fasto/Fasto/obj/Debug/net8.0/Fasto.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+795473ececfa739da80d131f1bf1058f58db9a54")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b37eb700e768dfd10895b332ee6c80ff6c8480b7")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
